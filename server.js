ad1163be949536bd779637e64c6576089cfc127e
subhanali <117721076+Subhanali000@users.noreply.github.com>
2025-03-15 06:52:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,28 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const requestIp = require('request-ip');
-const couponRoutes = require('./routes/coupons'); 
+const couponRoutes = require('./routes/coupons');
 
 const app = express();
 
+// CORS Configuration
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow requests from the frontend (React app)
+  credentials: true, // Allow cookies to be sent with requests
+  methods: 'GET,POST,PUT,DELETE', // Allowed methods
+  allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+};
+
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true })); // Allow frontend to access
+app.use(cors(corsOptions)); // Use the CORS middleware with the specified options
 app.use(express.json());
 app.use(cookieParser());
-app.use(requestIp.mw()); 
+app.use(requestIp.mw());
 
 // Use routes
 app.use('/api/coupons', couponRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
